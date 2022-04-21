2e5106b00c887da135ebf7398561789283796c59
Luciano Pereira <luciano.pereiraaristimunho@gmail.com>
2022-04-20 22:31:32 -0300

    gulp-02

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 
 const gulp = require('gulp')
-const {series} = require('gulp')
+const {parallel, series} = require('gulp')
 const htmlmin = require('gulp-htmlmin')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
-const html = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 
 function tarefasCSS(cb) {
     return gulp.src([
-            '../node_modules/bootstrap/dist/css/bootstrap.css', 
-            '../vendor/OwlCarousel2-2.3.4/dist/assets/owl.carousel.css', 
-            '../node_modules/font-awesome/css/font-awesome.css',
-            'css/style.css',
+            './node_modules/bootstrap/dist/css/bootstrap.css', 
+            './vendor/OwlCarousel2-2.3.4/dist/assets/owl.carousel.css', 
+            './node_modules/font-awesome/css/font-awesome.css',
+            './src/css/style.css'
         ])
         .pipe(concat('libs.css'))
         .pipe(cssmin())
@@ -25,15 +27,17 @@ function tarefasCSS(cb) {
 
 function tarefasJS() {
     return gulp.src([
-        '../node_modules/jquery/dist/jquery.js',
-        '../node_modules/bootstrap/dist/js/bootstrap.js',
-        '../vendor/OwlCarousel2-2.3.4/dist/owl.carousel.js',
-        '.js/custom.js',
-    ])
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './vendor/OwlCarousel2-2.3.4/dist/owl.carousel.js',
+            './src/js/custom.js'
+        ])
+        
         .pipe(concat('libs.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'})) //libs.min.js
         .pipe(gulp.dest('./dist/js'))
+
 }
 
 function tarefasImagem(){
@@ -61,8 +65,29 @@ function tarefasHTML(callback) {
     return callback()
 }
 
+gulp.task('serve', function(){
 
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process) // repete o processo quando alterar algo em src
+    gulp.watch('./src/**/*').on('change', reload)
+
+})
+
+
+function end(cb){
+    console.log('Tarefas Concluídas')
+    return cb()
+}
+
+const process = series( tarefasHTML, tarefasJS, tarefasCSS, end)
+
+exports.default = process
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
-exports.images = tarefasImagem
 exports.html = tarefasHTML
+exports.images = tarefasImagem
+
